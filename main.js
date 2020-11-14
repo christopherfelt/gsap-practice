b367654901177f46703755c15fb2558a922d234e
christopherfelt <chris.h.felt@gmail.com>
2020-11-13 18:47:26 -0700

    drag work

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 const container = document.querySelector(".drag-container");
 const listTwo = document.getElementById("listTwo");
+
 Draggable.create("#drag-square", {
   bounds: container,
-  onDrag: function () {
-    if (this.hitTest(listTwo, 20)) {
-    }
-  },
 });
