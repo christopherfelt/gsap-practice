b8183e9c781232cb6871d57259707540db826163
christopherfelt <chris.h.felt@gmail.com>
2020-11-17 18:54:07 -0700

    fixed drag

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 const container = document.querySelector(".drag-container");
-const listTwo = document.getElementById("listTwo");
+const listTwo = document.getElementById("list-two");
+const moveTask = document.getElementById("drag-square");
+
+let taskInListTwo = false;
 
 Draggable.create("#drag-square", {
   bounds: container,
+  // liveSnap: {
+  //   x: [0, 50, 100, 150],
+  //   y: [0, 20, 40, 100],
+  //   // we can also use this notation
+  //   // points [{x: 0, y: 0}, {x: 30, y: 40}]
+  // },
+  onDrag: function () {
+    if (this.hitTest(listTwo, 10)) {
+      listTwo.style.border = "2px solid blue";
+      taskInListTwo = true;
+    } else {
+      listTwo.style.border = "1px solid black";
+      taskInListTwo = false;
+    }
+  },
+  onDragEnd: function () {
+    if (taskInListTwo) {
+      moveTask.style.display = "none";
+    }
+  },
 });
